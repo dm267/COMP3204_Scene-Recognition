257f98450e3dd9d30379e59019ad6a41f3e41176
DSGeorgiev <dsg1u18@soton.ac.uk>
2021-01-03 20:14:29 +0000

    separated the ExtractPatches class from the App.java

--- src/main/java/uk/ac/soton/ecs/dm4g17/run2/App.java
+++ src/main/java/uk/ac/soton/ecs/dm4g17/run2/App.java
@@ -1,14 +1,28 @@
 package uk.ac.soton.ecs.dm4g17.run2;
 
+import org.openimaj.data.DataSource;
+import org.openimaj.data.FloatArrayBackedDataSource;
 import org.openimaj.data.dataset.VFSGroupDataset;
 import org.openimaj.data.dataset.VFSListDataset;
+import org.openimaj.feature.FloatFV;
+import org.openimaj.feature.local.LocalFeature;
+import org.openimaj.feature.local.SpatialLocation;
+import org.openimaj.feature.local.data.LocalFeatureListDataSource;
+import org.openimaj.feature.local.list.LocalFeatureList;
+import org.openimaj.feature.local.list.MemoryLocalFeatureList;
 import org.openimaj.image.FImage;
 import org.openimaj.image.ImageUtilities;
+import org.openimaj.image.feature.local.keypoints.FloatKeypoint;
+import org.openimaj.ml.clustering.FloatCentroidsResult;
+import org.openimaj.ml.clustering.assignment.HardAssigner;
+import org.openimaj.ml.clustering.kmeans.FloatKMeans;
+import org.openimaj.util.pair.IntFloatPair;
 import org.springframework.util.ResourceUtils;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class App {
@@ -28,42 +42,41 @@ public class App {
 
     }
 
-    private static class ExtractPatches {
-        // size of each patch
-        int patchSize;
-        // space/distance between each patch
-        int patchDistance;
 
-        //constructor of the ExtractPatches class
-        public ExtractPatches (int patchSize, int patchDistance)
-        {
-            this.patchSize = patchSize;
-            this.patchDistance = patchDistance;
-        }
 
         /*
-        Split FImage into patches.
-        returns a List of normalised patches from the input image.
+        perform K-Means clustering in order to build
+        a HardAssigner that can assign features to identifiers.
+        Note: Some of the code is reused from tutorial 12
          */
-        public List<FImage> getExtractedPatches(FImage image)
+        private static HardAssigner<float[], float[], IntFloatPair> trainQuantiser
+                (VFSGroupDataset<FImage> dataset, ExtractPatches extractPatches)
         {
-            List<FImage> patches = new ArrayList<>();
-            //height of the image
-            int imageHeight = image.getHeight();
-            //width of the image
-            int imageWidth = image.getWidth();
-
-            for (int row = 0; row < imageHeight; row += patchDistance)
+            LocalFeatureList<FloatKeypoint> allKeys =
+                     new MemoryLocalFeatureList<>();
+            for(FImage img: dataset)
             {
-                for (int col = 0; col < imageWidth; col += patchDistance)
+                LocalFeatureList<FloatKeypoint> keypointList = new MemoryLocalFeatureList<>();
+                List<FImage> patchesOfImage = extractPatches.getExtractedPatches(img);
+                for (FImage patch : patchesOfImage)
                 {
-                    FImage patch = image.extractROI(row, col, patchSize, patchSize);
-                    //normalise
-                    patch = patch.normalise();
-                    patches.add(patch);
+                    //not sure if I got the right float vector of the image
+                    FloatKeypoint floatKeypoint =
+                            new FloatKeypoint(0, 0, 0, 0, patch.getFloatPixelVector());
+                    keypointList.add(floatKeypoint);
                 }
+                allKeys.addAll(keypointList);
             }
-            return patches;
+            if (allKeys.size() > 10000)
+            {
+                allKeys = allKeys.subList(0, 10000);
+            }
+
+            //clusters the features into 500 separate classes
+            FloatKMeans km = FloatKMeans.createKDTreeEnsemble(500);
+            DataSource<float[]> datasource = new LocalFeatureListDataSource<>(allKeys);
+            FloatCentroidsResult result = km.cluster(datasource);
+            return result.defaultHardAssigner();
         }
     }
-}
+
---
+++ src/main/java/uk/ac/soton/ecs/dm4g17/run2/ExtractPatches.java
@@ -0,0 +1,41 @@
+package uk.ac.soton.ecs.dm4g17.run2;
+
+import org.openimaj.image.FImage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExtractPatches {
+    // size of each patch
+    int patchSize;
+    // space/distance between each patch
+    int patchDistance;
+
+    //constructor of the ExtractPatches class
+    public ExtractPatches(int patchSize, int patchDistance) {
+        this.patchSize = patchSize;
+        this.patchDistance = patchDistance;
+    }
+
+    /*
+    Split FImage into patches.
+    returns a List of normalised patches from the input image.
+     */
+    public List<FImage> getExtractedPatches(FImage image) {
+        List<FImage> patches = new ArrayList<>();
+        //height of the image
+        int imageHeight = image.getHeight();
+        //width of the image
+        int imageWidth = image.getWidth();
+
+        for (int row = 0; row < imageHeight; row += patchDistance) {
+            for (int col = 0; col < imageWidth; col += patchDistance) {
+                FImage patch = image.extractROI(row, col, patchSize, patchSize);
+                //normalise
+                patch = patch.normalise();
+                patches.add(patch);
+            }
+        }
+        return patches;
+    }
+}
