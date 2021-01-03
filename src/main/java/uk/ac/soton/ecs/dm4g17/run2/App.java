deb80a0152d08bc4c8c7728338e6ce34d5d6f27a
DSGeorgiev <dsg1u18@soton.ac.uk>
2021-01-03 15:03:30 +0000

    added ExtractPatches class to run2

--- src/main/java/uk/ac/soton/ecs/dm4g17/run2/App.java
+++ src/main/java/uk/ac/soton/ecs/dm4g17/run2/App.java
@@ -8,6 +8,8 @@ import org.springframework.util.ResourceUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
     public static void main( String[] args ) throws IOException {
@@ -24,5 +26,44 @@ public class App {
         VFSGroupDataset<FImage> trainingData = new VFSGroupDataset<>(trainFile.getPath(), ImageUtilities.FIMAGE_READER);
         VFSListDataset<FImage> testingData = new VFSListDataset<>(testFile.getPath(), ImageUtilities.FIMAGE_READER);
 
+    }
+
+    private static class ExtractPatches {
+        // size of each patch
+        int patchSize;
+        // space/distance between each patch
+        int patchDistance;
+
+        //constructor of the ExtractPatches class
+        public ExtractPatches (int patchSize, int patchDistance)
+        {
+            this.patchSize = patchSize;
+            this.patchDistance = patchDistance;
+        }
+
+        /*
+        Split FImage into patches.
+        returns a List of normalised patches from the input image.
+         */
+        public List<FImage> getExtractedPatches(FImage image)
+        {
+            List<FImage> patches = new ArrayList<>();
+            //height of the image
+            int imageHeight = image.getHeight();
+            //width of the image
+            int imageWidth = image.getWidth();
+
+            for (int row = 0; row < imageHeight; row += patchDistance)
+            {
+                for (int col = 0; col < imageWidth; col += patchDistance)
+                {
+                    FImage patch = image.extractROI(row, col, patchSize, patchSize);
+                    //normalise
+                    patch = patch.normalise();
+                    patches.add(patch);
+                }
+            }
+            return patches;
+        }
     }
 }
