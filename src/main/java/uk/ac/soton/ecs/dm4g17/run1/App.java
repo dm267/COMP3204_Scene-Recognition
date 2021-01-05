b06680720fa67ad0e475c369da9f7994d4513dea
Dylan Maguire <dm4g17@soton.ac.uk>
2021-01-05 20:48:51 +0000

    Run 1 added

--- src/main/java/uk/ac/soton/ecs/dm4g17/run1/App.java
+++ src/main/java/uk/ac/soton/ecs/dm4g17/run1/App.java
@@ -1,22 +1,42 @@
 package uk.ac.soton.ecs.dm4g17.run1;
 
-import org.apache.commons.vfs2.FileSystemException;
+import org.apache.commons.vfs2.FileObject;
 
+import org.apache.commons.vfs2.FileSystemException;
+import org.openimaj.data.dataset.GroupedDataset;
+import org.openimaj.data.dataset.ListDataset;
 import org.openimaj.data.dataset.VFSGroupDataset;
 import org.openimaj.data.dataset.VFSListDataset;
 import org.openimaj.experiment.dataset.split.GroupedRandomSplitter;
+import org.openimaj.experiment.evaluation.classification.ClassificationEvaluator;
+import org.openimaj.experiment.evaluation.classification.ClassificationResult;
+import org.openimaj.experiment.evaluation.classification.analysers.confusionmatrix.CMAnalyser;
+import org.openimaj.experiment.evaluation.classification.analysers.confusionmatrix.CMResult;
+import org.openimaj.feature.DoubleFV;
+import org.openimaj.feature.DoubleFVComparison;
 import org.openimaj.feature.FeatureExtractor;
 import org.openimaj.feature.FloatFV;
 import org.openimaj.feature.FloatFVComparison;
 import org.openimaj.image.FImage;
 import org.openimaj.image.ImageUtilities;
 import org.openimaj.image.processing.resize.ResizeProcessor;
+import org.openimaj.ml.annotation.Annotated;
 import org.openimaj.ml.annotation.basic.KNNAnnotator;
 import org.springframework.util.ResourceUtils;
 
+import afu.org.checkerframework.checker.units.qual.K;
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Map;
+
 
 public class App {
 
@@ -33,58 +53,104 @@ public class App {
 
     public static void main( String[] args ) throws IOException {
 
+        App run1 = new App();
+        run1.run();
+    }
+
+    App() {
+    }
+
+    public void run() throws IOException {
         System.out.println("Run 1 Initiated...");
 
-        //Retrieving files
-        File trainingFile = ResourceUtils.getFile("classpath:training.zip");
-        File testingFile = ResourceUtils.getFile("classpath:testing.zip");
+        // Retrieving files
+        File trainingFile = ResourceUtils.getFile("classpath:training");
+        File testingFile = ResourceUtils.getFile("classpath:testing");
 
-        //Files are found
+        // Files are found
         System.out.println("File Found : " + trainingFile.exists());
         System.out.println("File Found : " + testingFile.exists());
 
-        //Adding files to VFSDatasets
+        // Adding files to VFSDatasets
         VFSGroupDataset<FImage> trainingData = new VFSGroupDataset<>(trainingFile.getPath(), ImageUtilities.FIMAGE_READER);
         VFSListDataset<FImage> testingData = new VFSListDataset<>(testingFile.getPath(), ImageUtilities.FIMAGE_READER);
-		
-        //Not sure what parameters to pass this so I just put 0,0,0 for now
-        //GroupedRandomSplitter<String, FImage> splitter = new GroupedRandomSplitter<String,FImage>(trainingData, 0, 0, 0);
+
+        // Used to ensure that there are 1500 images in the training set & 2985 testing images
+        //System.out.println(trainingData.numInstances());
+        //System.out.println(testingData.numInstances());
+
+        // Needed otherwise training set appears to be only 15 due to scene sub-folders
+//       ArrayList<FImage> trainingImages = new ArrayList<FImage>();
+//        ArrayList<String> trainingScenes = new ArrayList<String>();
+
+        // Loop through images/scenes within sub directories and add them to an ArrayList
+//             for(String scenes : trainingData.getGroups()) {
+//
+//        	    trainingScenes.add(scenes);
+//
+//       // Adds every image found in each sub directory within the training folder to an ArrayList
+//       	    for(FImage image : trainingData.getInstances(scenes)) {
+//        		trainingImages.add(image);
+//       	}
+//        }
+
+        //System.out.println(trainingData.getGroups().size());
+        //System.out.println(testingData.size());
+        //System.out.println(trainingImages.size());
+        //System.out.println(trainingScenes.size());
 
 
-		// Was thinking of creating an instance of a new TinyImage class but maybe the method at the bottom is calm?
-        //FeatureExtractor<FloatFV, FImage> tinyImage = createTinyImage(,16);
+        // Split the training & testing datasets into training, validation and testing subsets
+        GroupedRandomSplitter<String, FImage> splitter = new GroupedRandomSplitter<String,FImage>(trainingData, 80, 0, 20);
+        GroupedDataset<String, ListDataset<FImage>, FImage> testSet  = splitter.getTestDataset();
 
-        //Number of different scene classes to decide from
+        // Creating an instance of the TinyImage class
+        // Allows us to extract the centre of the images for resizing to 16x16
+        FeatureExtractor<DoubleFV, FImage> tinyImage = new TinyImage();
+
+        //Number of k neighbours , a parameter of KNNAnnotator
         int knnNumber = 15;
-        //Size of tinyImage is 16*16, a 16 bit representation of original image
-        int tinyImageSize = 16;
-        
-        //KNNAnnotator<FImage, String, FloatFV> knn = new KNNAnnotator<FImage, String, FloatFV>(tinyImage, FloatFVComparison.EUCLIDEAN, knnNumber);
-		//knn.train(trainingData);
 
-        FImage oldImage = null;
-        FImage newImage = null;
-        newImage = createTinyImage(oldImage, tinyImageSize);
+        // Performs the knn search using the chosen k number
+        // Trains the KNNAnnotator on the datsets with the chosen subset splits
+        KNNAnnotator<FImage, String, DoubleFV> knn = new KNNAnnotator<FImage, String, DoubleFV>(tinyImage, DoubleFVComparison.EUCLIDEAN, knnNumber);
+        knn.train(splitter.getTrainingDataset());
 
 
+        // Find and print the accuracy of our KNN classifier
+        // Help from chapter 12 of OpenIMAJ tutorial
+        ClassificationEvaluator<CMResult<String>, String, FImage> knnResult = new ClassificationEvaluator<CMResult<String>, String, FImage>(
+                knn, testSet, new CMAnalyser<FImage, String>(CMAnalyser.Strategy.SINGLE));
+
+        System.out.println("Starting evaluation");
+        Map<FImage, ClassificationResult<String>> knnGuesses = knnResult.evaluate();
+
+        System.out.println("Analysing results");
+        CMResult<String> knnScore = knnResult.analyse(knnGuesses);
+        System.out.println(knnScore.getDetailReport());
 
 
+        // Creates a file named run1.txt where we write our predictions for each run
+        File run1File = new File ("/COMP3204_CW3/run1.txt");
+        PrintWriter txtPrinter = new PrintWriter("run1.txt");
 
+        // Allows us to iterate through the testing dataset classes
+        String result;
+        FileObject[] files = testingData.getFileObjects();
 
+        // Iterate through the training images and run our classifier on them
+        // Print our results to the created run1.txt file
+        for(int i=0; i < testingData.size(); i++) {
+            result = files[i].getName().getBaseName()+" "+knn.classify(testingData.get(i)).getPredictedClasses().iterator().next();
 
+            txtPrinter.println(result);
+
+        }
+        // Close the stream
+        txtPrinter.close();
     }
 
-    //Auxiliary function to convert image to tinyImage version for further processing
-    public static FImage createTinyImage(FImage image, int newSize) {
-        int startingX = (image.width);
-        int startingY = (image.height);
-        int smallestDimension = Math.min(startingX, startingY);
-
-        //This will extract a square version of the image that extends width/2 and height/2 from the centre point
-        //Uses smallest dimension of image as we are cropping the image and cant add any information to enlarge it
-        FImage squareVersionOfImage = image.extractCenter(smallestDimension,smallestDimension);
-
-        //Resizes to the passed required size
-        return squareVersionOfImage.process(new ResizeProcessor(newSize, newSize));
-    }
 }
+
+
+
